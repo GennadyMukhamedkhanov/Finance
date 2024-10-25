53ad2b734a848ed934b22eefc860021a87863b58
GennadyMukchamedchanov <dungreshmen2@mail.ru>
2024-10-25 18:10:03 +0300

    modified urls pipfile .gitignore

--- finance/api/urls.py
+++ finance/api/urls.py
@@ -6,11 +6,12 @@ from api.views.user import CreateListUsersView, GetPutDeleteUserView
 urlpatterns = [
 
     # User
-    path('users/', CreateListUsersView.as_view()),
+    path('users/', CreateListUsersView.as_view(), name='create_list_users'),
     path('user/<int:id>/', GetPutDeleteUserView.as_view()),
 
     # Transaction
     path('transactions/', CreateListTransactionsView.as_view()),
-    path('transactions/<int:id>/', GetPutDeleteTransactionsView.as_view()),
+    path('transaction/<int:id>/', GetPutDeleteTransactionsView.as_view()),
+
 
 ]
